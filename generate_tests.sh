002421c13a2ebfa41c61cab66a9d73ec667c08f0
Reece Adamson <radamson@mitre.org>
2020-08-05 08:13:15 -0400

    update branch used

--- generate_tests.sh
+++ generate_tests.sh
@@ -2,7 +2,8 @@
 
 sushi .
 rm -rf inferno
-git clone -b FI-886 --single-branch https://github.com/onc-healthit/inferno.git
+git clone -b fi-898-900-928 --single-branch https://github.com/onc-healthit/inferno.git
+# git clone -b development --single-branch https://github.com/onc-healthit/inferno.git
 cp -r build/input/profiles inferno/resources/demo
 cd inferno
 bundle install
